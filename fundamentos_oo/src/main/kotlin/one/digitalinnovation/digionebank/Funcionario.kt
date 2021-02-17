92e84dc9749e26378e190f0364685e1d244f6473
Tiago Ferrari <tiagoferrari77@gmail.com>
2021-02-16 23:35:07 -0300

    updated employed class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -1,11 +1,16 @@
 package one.digitalinnovation.digionebank
 
-import java.math.BigDecimal
-
 abstract class Funcionario(
     nome: String,
     cpf: String,
-    val salario: BigDecimal
+    val salario: Double
 ):Pessoa(nome,cpf){
+    protected abstract fun calculoAuxilio(): Double
 
+    override fun toString(): String = """
+        Nome: $nome
+        CPF: $cpf
+        Salário: R$ $salario
+        Auxílio: R$ ${calculoAuxilio()}
+ """.trimIndent()
 }
